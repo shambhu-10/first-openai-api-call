bed82421e20fae120f88626f50eaa4f5a897a137
shambhu <shambhu04.kumar@gmail.com>
2025-05-28 01:09:45 +0530

    add the first_call.py foler

---
+++ first_call.py
@@ -0,0 +1,54 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import openai 
+import os
+
+############    for multi-time response (chatting)
+
+client = openai.OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+messages = [{"role": "system", "content": "You are a helpful assistant."}]
+
+print("NOTE: Enter 'exit or quit' to terminate the chat")
+while True:
+    
+    user_input = input("User: ")
+    if user_input.lower() in ["exit", "quit"]:
+        break
+
+    messages.append({"role": "user", "content": user_input})
+
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        temperature=1,
+        max_tokens=100
+    )
+
+    assistant_reply = response.choices[0].message.content
+    token_count = response.usage.total_tokens
+
+    print("Assistant:", assistant_reply, ("Token count:", token_count))
+    # print("Token count:", token_count)
+
+    messages.append({"role": "assistant", "content": assistant_reply})
+
+
+#######################      for one time response: 
+
+# user_input = input("Ask something: ")
+
+# response = client.chat.completions.create(
+#     model="gpt-3.5-turbo",
+#     messages=[
+#         {"role": "system", "content": "You are a helpful assistant."},
+#         {"role": "user", "content": user_input}
+#     ],
+#     temperature=1,
+#     max_tokens=100
+# )
+# answer = response.choices[0].message.content
+# print("Assistant: ", answer)
+
+# print("Total tokens used: ", response.usage.total_tokens)
